f16bcf7f51d9bb2d5d1446f2711c4c1d48a3fe66
Arseni Shcherbik <Arseni.Shcherbik@microsoft.wsei.edu.pl>
2022-03-10 11:35:43 +0100

    Lab_01

--- Lab_01/Lab_01/Lab_01/Program.cs
+++ Lab_01/Lab_01/Lab_01/Program.cs
@@ -6,7 +6,30 @@ namespace Lab_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Ułamek ułamek = new Ułamek();
+            Ułamek ułamek1 = new Ułamek();
         }
     }
+    class Ułamek
+    {
+        private int licznik;
+        private int mianownik;
+
+        public Ułamek()
+        {
+            this.licznik = 0;
+            this.mianownik = 1;
+        }
+        public Ułamek(Ułamek A_U)
+        {
+            this.licznik = A_U.licznik;
+            this.mianownik = A_U.mianownik;
+        }
+        public override string ToString()
+        {
+            return "licznik=" + this.licznik + " mianowk=" + this.mianownik;
+        }
+    }
+
+
 }
